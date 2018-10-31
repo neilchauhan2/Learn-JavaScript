6abe7153fd1da6de2c400cee41a5ea8a559cb7ca
RanveerSinghKhattra <37954826+RanveerSinghKhattra@users.noreply.github.com>
2018-10-31 20:09:18 +0530

    Create every.js

---
+++ every.js
@@ -0,0 +1,7 @@
+//Every function returns true if the given function returns true for all the array elements.
+var val1 = [1,2,3].every(val=>val>0);
+var val 2= [1,2,3].every(val=>val>4);
+var val3 = [1,2,3].every(val=>val>2);
+console.log(val1);  //true
+console.log(val2);  //false
+console.log(val3);  //false
